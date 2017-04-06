4b32cfc2ee2c380d3d428147feb3ffcf48f674bc
Ryan Pierce <ryan.pierce@buildium.com>
2017-04-06 09:06:20 -0400

    Removing Angular references that are no longer needed

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 'use strict';
 
-let moduleName = 'buildium.angular-redux';
-
-/**
- * @ndgoc module
- * @name angular-redux
- * @module angular-redux
- */
-angular.module(moduleName, [
-    require('./src/utils')
-]);
-
-module.exports = moduleName;
-
-module.exports = require('./src/utils/index')
+module.exports = require('./src/utils/index');
